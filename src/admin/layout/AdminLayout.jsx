2c8145d7d3aeefff6c199d4dd1fc7026e0ed2a00
Muhammadali <132482516+muhammadali-amor@users.noreply.github.com>
2025-03-02 23:49:27 +0500

    nastroy admin panel

---
+++ src/admin/layout/AdminLayout.jsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import {createTheme} from '@mui/material/styles';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Groups3Icon from '@mui/icons-material/Groups3';
+import {AppProvider} from '@toolpad/core/AppProvider';
+import {DashboardLayout} from '@toolpad/core/DashboardLayout';
+import {Outlet, useNavigate} from "react-router-dom";
+import { GiBookshelf } from "react-icons/gi";
+
+const NAVIGATION = [
+    {
+        kind: 'header',
+        title: 'Main items',
+    },
+    {
+        segment: 'dashboard',
+        title: 'Dashboard',
+        icon: <DashboardIcon/>,
+    },
+    {
+        segment: 'dashboard/users',
+        title: 'Users',
+        icon: <Groups3Icon/>,
+    },
+    {
+        segment: 'dashboard/books',
+        title: 'Kitoblar',
+        icon: <GiBookshelf />,
+    },
+];
+
+const demoTheme = createTheme({
+    cssVariables: {
+        colorSchemeSelector: 'data-toolpad-color-scheme',
+    },
+    colorSchemes: {light: true, dark: true},
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 600,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
+});
+
+
+export const AdminLayout = (props) => {
+    const n = useNavigate()
+    const {window} = props;
+
+    const [pathname, setPathname] = React.useState('/dashboard');
+    const router = React.useMemo(() => {
+        return {
+            pathname,
+            searchParams: new URLSearchParams(),
+            navigate: (path) => {
+                setPathname(String(path))
+                n(path)
+            },
+        };
+    }, [pathname]);
+
+    const demoWindow = window !== undefined ? window() : undefined;
+
+    let tok = sessionStorage.getItem("__token?_")
+    tok = "oo"
+    return (
+        tok != null ? <AppProvider
+            navigation={NAVIGATION}
+            router={router}
+            theme={demoTheme}
+            window={demoWindow}
+        >
+            <DashboardLayout>
+                <Outlet/>
+            </DashboardLayout>
+        </AppProvider> : n('/auth/login')
+    );
+}
+
+AdminLayout.propTypes = {
+    window: PropTypes.func,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+import 'uikit/dist/css/uikit.min.css';
+import 'uikit/dist/js/uikit.min.js';
+import 'uikit/dist/js/uikit-icons.min.js';
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+      <ToastContainer/>
   </StrictMode>,
 )
