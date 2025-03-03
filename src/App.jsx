7fbdbeacf2abbb34c48c8ecd36b2180a3cc4fc27
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2025-03-03 21:35:08 +0500

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AdminLayout} from "./admin/layout/AdminLayout.jsx";
-import AdminDashboard from "./admin/component/AdminDashboard.jsx";
-import UserLayout from "./user/layout/UserLayout.jsx";
-import UserMenu from "./user/component/UserMenu.jsx";
+// import {AdminDashboard} from "./admin/components/AdminDashboard.jsx";
+// import {UserLayout} from "./user/layout/UserLayout.jsx";
+// import {UserMenu} from "./user/component/UserMenu.jsx";
+import { Login } from "./admin/pages/Login.jsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/admin-dashboard"} element={<AdminLayout/>}>
-                    <Route index element={<AdminDashboard/>}/>
+                <Route path="/login" element={<Login/>}/>
+                 <Route path={"/admin-dashboard"} element={<AdminLayout/>}>
+                    {/* <Route index element={<AdminDashboard/>}/> */}
                 </Route>
-                <Route path={"/"} element={<UserLayout/>}>
-                    <Route index element={<UserMenu/>}/>
-                </Route>
+                {/* <Route path={"/"} element={<UserLayout/>}> */}
+                    {/* <Route index element={<UserMenu/>}/> */}
+                {/* </Route> */}
             </Routes>
         </BrowserRouter>
     )
---
+++ src/admin/pages/Login.jsx
@@ -0,0 +1,26 @@
+import '../assets/css/login.css'
+export const Login =()=>{
+    return(
+        <div className='login-background'>
+            <div className='login-container'>
+                <h1>Login</h1>
+                <form>
+                    <div className='input-box'>
+                    <input type="text" placeholder='email'/>
+                    </div>
+                    <div className='input-box'>
+                        <input type="text" placeholder='password'/>
+                    </div>
+                    <div className='remember-forgot'>
+                        <label><input type="checkbox"/>Rember me</label>
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button className='btn'>Login</button>
+                    <div className='register-link'>
+                        <p>Dont have account? <a href="">Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
