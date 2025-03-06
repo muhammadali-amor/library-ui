9f6e70ced23502426719dc4763f42ab38317bea5
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2025-03-06 23:26:59 +0500

    v1

--- src/user/component/UserMenu.jsx
+++ src/user/component/UserMenu.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CustomCarousel from "./Carusell.jsx";
+// import CustomCarousel from "./Carusell.jsx";
 
 export const UserMenu = () => {
     return (
         <div>
-            <CustomCarousel/>
+            {/* <CustomCarousel/> */}
             UserMenu Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid beatae consequatur culpa
             cumque deleniti, distinctio id illum impedit incidunt ipsa ipsum iste minima non placeat repellat totam,
             voluptas voluptatem.
